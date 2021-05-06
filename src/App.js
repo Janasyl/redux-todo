1580d83eeb961679d67907e016e1012886320a97
[Janasyl] <jduishenbae@gmail.com>
2021-05-06 14:36:24 +0600

    Apatch redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function App {
+  const dispatch = useDispatch();
+  const items = useSelector(state => state);
+
+  function onAddCallback(event)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Redux Input example</h1>
+
+      <form>
+        <input type="text"  name="text"placeholder="Please your text" required/>
+        <button>Add</button>
+      </form>
     </div>
   );
 }
---
+++ src/redux/reducer/todo.js
@@ -0,0 +1,8 @@
+const initialState = {};
+
+const todo = (state - initialState, action) => {
+     switch (action.type) {
+          case "ADD_TODO":
+               const newState = { ...state };
+               }
+}
